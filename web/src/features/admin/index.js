48d1f93ddea7a5fb66fa1fc1ff5b20e567587708
Leander Jhairo Ponce <leander.ponce.dev@gmail.com>
2026-02-08 18:16:41 +0800

    Delete web directory

--- web/src/features/admin/index.js
+++
@@ -1,9 +0,0 @@
-// Admin feature exports
-export { default as AdminLoginPage } from "./pages/AdminLoginPage";
-export { default as Dashboard } from "./pages/Dashboard";
-export { default as InquiriesPage } from "./pages/InquiriesPage";
-export { default as ReservationsPage } from "./pages/ReservationsPage";
-export { default as TenantsPage } from "./pages/TenantsPage";
-export { default as TenantDetailsPage } from "./pages/TenantDetailsPage";
-export { default as RoomAvailabilityPage } from "./pages/RoomAvailabilityPage";
-export { default as ReportsPage } from "./pages/ReportsPage";
--- web/src/features/tenant/index.js
+++
@@ -1,6 +0,0 @@
-// Tenant Pages
-export { default as CheckAvailabilityPage } from "./pages/CheckAvailabilityPage";
-export { default as TenantDashboard } from "./pages/CheckAvailabilityPage"; // Legacy alias for backward compatibility
-export { default as ProfilePage } from "./pages/ProfilePage";
-export { default as BillingPage } from "./pages/BillingPage";
-export { default as ContractsPage } from "./pages/ContractsPage";
